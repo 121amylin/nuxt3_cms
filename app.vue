4871ba1e75dd351f62b4fa3112cb762d2d998003
121amylin <121amylin@gmail.com>
2023-01-02 22:26:23 +0800

    feat: feat: vup y/ UI工具_naiveui

--- app.vue
+++ app.vue
@@ -1,5 +1,42 @@
+//
 <template>
   <div>
-    <NuxtWelcome />
+    <n-result status="success" title="succss" description="Naive-ui + Nuxt rc1">
+      <template #footer>
+        <n-space justify="center">
+          <n-button type="success">Naive-ui</n-button>
+          <n-button type="success" secondary>+</n-button>
+          <n-button type="success">Nuxt rc1</n-button>
+        </n-space>
+      </template>
+    </n-result>
+    <n-space>
+      <n-button type="primary">naive-ui</n-button>
+      <n-button type="info">naive-ui</n-button>
+      <n-button type="warning">naive-ui</n-button>
+    </n-space>
+    <n-h2>Tooltip demo</n-h2>
+    <n-tooltip trigger="hover">
+      <template #trigger>
+        <n-button>鸭子</n-button>
+      </template>
+      如果它长得像鸭子，走起来像鸭子，叫起来也像鸭子，那它一定是个鸭子。
+    </n-tooltip>
+    <n-h1>特点</n-h1>
+    <n-space>
+      <n-tag>Nuxt3</n-tag>
+      <n-tag>Vite</n-tag>
+      <n-tag>Naive-ui</n-tag>
+      <n-tag>SEO friendly</n-tag>
+    </n-space>
+
+
+      <n-space>
+        <n-tag> 爱在西元前 </n-tag>
+        <n-tag type="success"> 不该 </n-tag>
+        <n-tag type="warning"> 超人不会飞 </n-tag>
+        <n-tag type="error"> 手写的从前 </n-tag>
+        <n-tag type="info"> 哪里都是你 </n-tag>
+      </n-space>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,24 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+// import { defineNuxtConfig } from 'nuxt'
+import Components from 'unplugin-vue-components/vite'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
+// https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-
+  build: {
+    transpile: ['vueuc']
+  },
+  vite: {
+    plugins: [
+      Components({
+        resolvers: [NaiveUiResolver()]
+      })
+    ],
+    ssr: {
+      noExternal: [
+        'moment',
+        'naive-ui',
+        '@juggle/resize-observer',
+        '@css-render/vue3-ssr'
+      ]
+    }
+  }
 })
