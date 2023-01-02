71e2ac5e60b62efc165460675d649c88303ab37c
121amylin <121amylin@gmail.com>
2023-01-02 23:37:33 +0800

    fix: 調整UI工具naivw-ui.ts plugins 設定

--- plugins/naive-ui.ts
+++ plugins/naive-ui.ts
@@ -5,13 +5,13 @@ import { defineNuxtPlugin } from '#app'
 
 export default defineNuxtPlugin((nuxtApp) => {
   if (process.server) {
-    const { collect } = setup(nuxtApp.vueApp)
-    const originalRender = nuxtApp.ssrContext.renderMeta
-    nuxtApp.ssrContext!.renderMeta = () => {
-      const result = originalRender()
-      return {
-        headTags: result['headTags'] + collect() // 组合样式跟head信息
-      }
-    }
+    // const { collect } = setup(nuxtApp.vueApp)
+    // const originalRender = nuxtApp.ssrContext.renderMeta
+    // nuxtApp.ssrContext!.renderMeta = () => {
+    //   const result = originalRender()
+    //   return {
+    //     headTags: result['headTags'] + collect() // 组合样式跟head信息
+    //   }
+    // }
   }
 })
